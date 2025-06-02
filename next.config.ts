5cb4fea12c121f359f7074d98bf446d3a157da27
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-06-02 06:35:29 +0300

    Config (added): relative pathes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias ?? {}),
+      "@app": path.resolve(__dirname, "app"),
+    };
+    return config;
+  }
 };
 
 export default nextConfig;
